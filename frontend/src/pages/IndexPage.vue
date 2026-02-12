38c6677ebe37e548e4867f8e991861a5c9a719b0
Sarayut380 <zusuchang@gmail.com>
2026-02-12 12:05:18 +0700

    feat: integrate quasar frontend with backend api using axios

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,15 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center column">
+    <h4>Backend API Demo</h4>
+
+    <q-btn label="Call API" color="primary" @click="callApi" />
+
+    <div v-if="response" class="q-mt-md">
+      <pre>{{ response }}</pre>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage'
-});
+import { ref } from 'vue'
+import axios from 'axios'
+
+const response = ref(null)
+
+const callApi = async () => {
+  try {
+    const res = await axios.get(
+      import.meta.env.VITE_API_URL + '/api/demo'
+    )
+    response.value = res.data
+  } catch (err) {
+    response.value = err.message
+  }
+}
 </script>
